17d3ff87f64596556780d8e6f20ebba7f3c11b0b
Kosuke Fujita <kousuke.f0807@gmail.com>
2019-11-28 11:38:56 +0000

    put URL in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+ # https://fierce-reaches-09141.herokuapp.com/hello/index
