9417ebc377e7c35ed4b618304dc26f0ead01f3d9
Kosuke Fujita <kousuke.f0807@gmail.com>
2019-12-22 07:18:51 +0000

    ranking func complete without design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,31 @@
 //= require Chart.min
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
+
+$(function(){
+    $(".home-total-ave-circle").addClass("hide-area");
+    $(".home-total-ave-circle").eq(0).removeClass("hide-area");
+    $(".rank-table").addClass("hide-area");
+    $(".rank-table").eq(0).removeClass("hide-area");
+    $(".test-type-name").addClass("hide-area");
+    $(".test-type-name").eq(0).removeClass("hide-area");
+    
+    $(".test-type-list").click(function(){
+        var listIndex = $(".test-type-list").index($(this));
+        $(".home-total-ave-circle").addClass("hide-area");
+        $(".rank-table").addClass("hide-area");
+        $(".test-type-name").addClass("hide-area");
+        // listIndex = listIndex + 1
+        console.log(listIndex);
+        $(".home-total-ave-circle").eq(listIndex).toggleClass("hide-area");
+        $(".rank-table").eq(listIndex).toggleClass("hide-area");
+        $(".test-type-name").eq(listIndex).toggleClass("hide-area");
+        
+        $(".test-type-list").removeClass("active-type");
+        $(this).addClass("active-type");
+        $("type-list-1").removeClass("hide-area");
+    });
+});
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,5 +3,53 @@ class TopController < ApplicationController
     @center_date = Date.new(2020, 1, 18)
     @date = @center_date - Date.current
     @date_of_examination = @date.to_i
+    
+    @scores = []
+    @type_scores = {}
+    
+    @type_names = TestType.all
+    
+    @test_types = TestType.all
+    
+    # TestType.find_by(id: 1).tests.each do |test|
+    #     test.scores.each do |score|
+    #       @scores.push(score.score)
+    #     end
+    # end
+    @test_types.each do |type|
+      type.tests.each do |test|
+        test.scores.each do |score|
+          @scores.push(score.score)
+        end
+      end
+      @type_scores[type.id] = @scores
+      @scores = []
+    end
+
+    
+    @total = 0
+    @type_ave_scores = {}
+    @type_scores.each do |i, val|
+      val.each do |score|
+        @total = @total + score
+      end
+      @ave_score = @total / val.length
+      @type_ave_scores[i] = @ave_score
+      @total = 0
+      @ave_score = 0
+    end
+    # @test_types[1].each do |s|
+    #   @total = @total + s
+    # end
+    @test_types = {}
+     @test_types[1] = [2,2,2,2,2,2]
+     @test_types[2] = [2,2,2,2,2,2]
+     @test_types[3] = [2,2,2,2,2,2]
+     @test_types[4] = [2,2,2,2,2,2]
+    @ave_score = @total / 1
+    
+    @ranking_scores =  @scores.sort.reverse
   end
+  
 end
+
